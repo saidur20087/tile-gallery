e7b743b4a4d08edaf9fcaf88ac4e364603f69616
saidur20087 <saidur.20087@gmail.com>
2026-05-01 15:03:51 +0600

    create not-found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      
+      <h1 className="text-6xl font-bold text-red-500">404</h1>
+
+      <p className="text-xl mt-4">
+        Tile Not Found
+      </p>
+
+      <p className="text-gray-500 mt-2">
+        The page you are looking for does not exist.
+      </p>
+
+      <Link
+        href="/"
+        className="mt-6 px-5 py-2 bg-cyan-600 text-white rounded-lg"
+      >
+        Go Home
+      </Link>
+
+    </div>
+  );
+};
+
+export default NotFound;
